5661fb68c160c1ae827337ff3d771d0c35febf08
Ben Kiefer <kieferfam@msn.com>
2012-12-17 07:12:30 -0600

    Ben: getting activemq back to a broker only approach. console needs more work

--- puppet/modules/activemq/manifests/init.pp
+++ puppet/modules/activemq/manifests/init.pp
@@ -2,20 +2,14 @@
 
 class activemq($port="61616", $adminPort="8161") {	
 	$instanceDirectory = "/etc/activemq/instances-enabled/main"
-
-	# try and deploy the admin app
-#	file { "/usr/share/activemq/webapps/admin.war":
-#		source => "activemq/admin.war",
-#		require => Package["activemq"],
-#	}
 	
-	# install the package
+    # install the package
 	package { "activemq":
 		ensure => present,
 	}
 
 	# create a directory for your instance
-	file { "$instanceDirectory":
+	file { $instanceDirectory:
 		ensure => directory,
 		require => Package["activemq"],
 	}
@@ -26,21 +20,23 @@ class activemq($port="61616", $adminPort="8161") {
 		enable => true,
 		subscribe => File["$instanceDirectory/activemq.xml"],
 		require => Package["activemq"],
-		#require => "/usr/share/activemq/webapps/admin.war",
 	}
 
 	# drop the activemq config off after the jetty config
 	file { "$instanceDirectory/activemq.xml":
 		ensure => present,
 		content => template("activemq/activemq.xml.erb"),
-		require => File["$instanceDirectory/jetty.xml"],
+		require => File["$instanceDirectory"],
+
+#		moving to this
+#		require => File["$instanceDirectory/jetty.xml"],
 	}
 
 	# drop the jetty config off after the instance directory was created
-	file { "$instanceDirectory/jetty.xml":
-		ensure => present,
-		content => template("activemq/jetty.xml.erb"),
-		require => File["$instanceDirectory"],
-	}	
+#	file { "$instanceDirectory/jetty.xml":
+#		ensure => present,
+#		content => template("activemq/jetty.xml.erb"),
+#		require => File["$instanceDirectory"],
+#	}	
 		
 }
